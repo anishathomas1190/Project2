c3b5a73ec3f5adcf6ec50446cffa31d6735894ec
anishathomas1190 <anishathomas1190@gmail.com>
2023-03-16 11:49:12 +0530

    first commit

---
+++ src/main/java/project2/myproject2/testcode.java
@@ -0,0 +1,14 @@
+package project2.myproject2;
+
+public class testcode {
+
+	public static void main(String[] args) {
+		System.out.println("Inside main");
+	}
+	public void display() {
+		System.out.println("Inside display");
+		System.out.println("hey........");
+	}
+}
+//http://myJenkins:8080/
+//http://localhost:8080/ 
